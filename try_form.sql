e11f7565ee7fbc9bed17cccfaf869a4b351989fb
Prashad01 <93466156+Prashad01@users.noreply.github.com>
2024-02-26 22:29:15 +0530

    Update try_form.sql

--- try_form.sql
+++ try_form.sql
@@ -1,1 +1,68 @@
 
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 26, 2024 at 05:48 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `try_form`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `signup_form`
+--
+
+CREATE TABLE `signup_form` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `signup_form`
+--
+
+INSERT INTO `signup_form` (`id`, `username`, `email`, `password`) VALUES
+(3, 'Prashad', 'prashadnath27@gmail.com', '$2y$10$hwUXv.1wbABTVCd1Kc0wpOmgE6t9jMbRVgcjKmV5XiAG7FgtbGrLG');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `signup_form`
+--
+ALTER TABLE `signup_form`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `signup_form`
+--
+ALTER TABLE `signup_form`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
